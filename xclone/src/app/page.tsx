4f4a2b68ff2a2adb001450ab5258495114fd6559
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2024-05-02 23:06:56 -0300

    auth

--- xclone/src/app/page.tsx
+++ xclone/src/app/page.tsx
@@ -1,3 +1,5 @@
+import { createServerClient } from '@supabase/ssr'
+
 export default function Home () {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
